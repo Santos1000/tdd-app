89f261b68407a1c18844e454ec1d42ea46a36174
barbara santos <barbaransantos1@gmail.com>
2022-07-21 16:32:17 -0300

    customer rspec

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "Customers", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "link client list work?" do
+    visit(root_path)
+    expect(page).to have_link('Client')
+  end
+
+  scenario "link new client work?" do
+    visit(root_path)
+    expect(page).to have_content('Client list')
+    click_on('Clients')
+    expect(page).to have_link('Add Client')
+  end
+
+  scenario "link new client work?" do
+    visit(customers_path)
+    click_on('Add Client')
+    expect(page).to have_content('New client')
+  end
 end
